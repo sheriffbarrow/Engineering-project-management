df35a863a58c347e25fdf181cbed27527ff9b18d
¨sheriffbarrow¨ <smsbarrowjnr@gmail.com>
2023-05-23 11:05:30 +0000

    first commit

---
+++ src/main/java/com/javaweb/project/management/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.javaweb.project.management;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
